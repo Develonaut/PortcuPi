2cd88eede894c5ca521e8380f7378b7e2fdf824f
Ryan McHenry <ryanmmchenry@gmail.com>
2016-06-04 15:19:07 -0700

    updated xml helper and removed unneded code

--- private/js/server/helpers/xml_helpers.js
+++ private/js/server/helpers/xml_helpers.js
@@ -6,19 +6,9 @@ var fs = require('fs'),
 
 module.exports = {
   buildXML: function(obj) {
-    // var xml = [xml];
-
-    // console.log(builder);
-    // // var blah = builder.buildObject(xml);
-
-    // var obj = {name: "Super", Surname: "Man", age: 23};
-
     var obj = obj[0];
     var builder = new xml2js.Builder();
     var xml = builder.buildObject(obj);
-
-    // console.log(xml);
-
     return xml;
   }
 };
